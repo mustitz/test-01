ece923a9359745188c476acbe3fe028698688b55
Mustitz <mustitz@gmail.com>
2016-10-10 19:36:03 +0300

    Add main test project.

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,17 @@
 #include <test.h>
 
+using namespace ImgLib;
+
 int main()
 {
+    float matrix[] = {
+        0.01, 0.11, 0.01,
+        0.11, 0.72, 0.11,
+        0.01, 0.11, 0.01
+    };
+
+    Image test;
+    test.Create(10, 10, Image::FLOAT, Image::RGB);
+    test.Convolve_3x3(matrix);
     return 0;
 }
