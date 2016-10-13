4b08ed1b32bf57d58c6f8412500275c399d27313
Mustitz <mustitz@gmail.com>
2016-10-13 15:18:51 +0300

    Write test for scaling

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,13 @@
 #include <test.h>
 
+#include <stdio.h>
+
 using namespace ImgLib;
 
 #define W 5
 #define H 5
 
-int main()
+void testConvolution()
 {
     float matrix[] = {
         0.01, 0.11, 0.01,
@@ -39,20 +41,57 @@ int main()
 
     Image test;
     test.create(W, H, FLOAT, RGB);
-    for (int x=0; x<W; ++x)
-    for (int y=0; y<H; ++y) {
+    for (size_t x = 0; x < W; ++x)
+    for (size_t y = 0; y < H; ++y) {
         FloatRgb value = { .r = R[x][y], .g = G[x][y], .b = B[x][y] };
         test.setPixel(x, y, value);
     }
 
     test.convolve_3x3(matrix);
 
-    for (int x=0; x<W; ++x)
-    for (int y=0; y<H; ++y) {
+    for (size_t x = 0; x < W; ++x)
+    for (size_t y = 0; y < H; ++y) {
         FloatRgb value;
         test.getPixel(x, y, &value);
         printf("%9.6f %9.6f %9.6f\n", value.r, value.g, value.b);
     }
+}
 
+void testScaling()
+{
+    float image[5][10] = {
+        { 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9},
+        { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0},
+        { 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 0.9},
+        { 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 0.9, 0.8},
+        { 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 0.9, 0.8, 0.7}
+    };
+
+    Image test;
+    test.create(5, 10, FLOAT, GRAY);
+    for (size_t x = 0; x <  5; ++x)
+    for (size_t y = 0; y < 10; ++y) {
+        test.setPixel(x, y, image[x][y]);
+    }
+
+    test.scale(0.5);
+
+    printf("W = %lu\n", test.getWidth());
+    printf("H = %lu\n", test.getHeight());
+    for (size_t x = 0; x < test.getWidth(); ++x) {
+        for (size_t y = 0; y < test.getHeight(); ++y) {
+            FloatGrayscale value;
+            test.getPixel(x, y, &value);
+            printf("%9.6f", value);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    testConvolution();
+    printf("\n");
+    testScaling();
     return 0;
 }
